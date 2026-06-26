8afbd6db988809818997ccc47cdc12ee3a0853fa
iamory619 <uni.nira22@gmail.com>
2026-06-26 13:41:17 +0700

    edit code

--- apps/web/app/widget/magic-lantern/[id]/page.tsx
+++ apps/web/app/widget/magic-lantern/[id]/page.tsx
@@ -56,13 +56,9 @@ export default function MagicLanternWidget() {
   phoenix:
    "radial-gradient(circle at 50% 58%, rgba(147,51,234,0.42) 0%, rgba(168,85,247,0.26) 45%, rgba(216,180,254,0.16) 72%, transparent 100%)",
   rat:
-    "radial-gradient(circle at 50% 58%, rgba(147,51,234,0.42) 0%, rgba(168,85,247,0.26) 45%, rgba(216,180,254,0.16) 72%, transparent 100%)",
-
-    // "radial-gradient(circle at 50% 58%, rgba(251,146,60,0.42) 0%, rgba(245,158,11,0.26) 45%, rgba(120,53,15,0.16) 72%, transparent 100%)",
-
+    "radial-gradient(circle at 50% 58%, rgba(254, 240, 138, 0.6) 0%, rgba(253, 230, 138, 0.35) 45%, rgba(254, 252, 232, 0.2) 72%, transparent 100%)",
   cat:
     "radial-gradient(circle at 50% 58%, rgba(236,72,153,0.42) 0%, rgba(244,114,182,0.26) 45%, rgba(168,85,247,0.16) 72%, transparent 100%)",
-
   rabbit:
     "radial-gradient(circle at 50% 58%, rgba(56,189,248,0.42) 0%, rgba(14,165,233,0.26) 45%, rgba(99,102,241,0.16) 72%, transparent 100%)",
 };
@@ -119,20 +115,9 @@ const dustColor = DUST_COLOR[lantern] || DUST_COLOR.phoenix;
         duration: 3 + Math.random() * 2,
         orbit: 8 + Math.random() * 16,
       }),
-    ); 
-    
-
-    // const newDusts: Dust[] = Array.from({ length: 90 }).map((_, index) => ({
-    //   id: Date.now() + 5000 + index,
-    //   x: 20 + Math.random() * 170,
-    //   y: 55 + Math.random() * 65,
-    //   size: 1.3 + Math.random() * 2.4,
-    //   delay: Math.random() * 1.8,
-    //   duration: 2 + Math.random() * 2.5,
-    // }));
+    );     
 
     setGifts((prev) => [...prev, ...newGifts].slice(-160));
-    // setDusts(newDusts);
 
     setTimeout(() => {
       setShowMessage(false);
@@ -220,22 +205,7 @@ const dustColor = DUST_COLOR[lantern] || DUST_COLOR.phoenix;
   }}
 />
 
-          {/* {dusts.map((dust) => (
-            <span
-              key={dust.id}
-              className="animate-magic-dust absolute rounded-full"              
-              style={{
-                left: `${dust.x}px`,
-                top: `${dust.y}px`,
-                width: `${dust.size}px`,
-                height: `${dust.size}px`,
-                animationDelay: `${dust.delay}s`,
-                animationDuration: `${dust.duration}s`,
-                 background: dustColor,
-                  boxShadow: `0 0 10px ${dustColor}`,
-              }}
-            />
-          ))} */}
+          {}
 
           {gifts.map((gift) => (
             <img
